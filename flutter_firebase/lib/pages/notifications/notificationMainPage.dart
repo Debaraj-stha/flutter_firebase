ec4ea99aa3927741511181b6ee8c5a206a3bc329
Debaraj-stha <dstha221@gmail.com>
2023-09-19 21:03:39 +0545

    firebase messaging

--- flutter_firebase/lib/pages/notifications/notificationMainPage.dart
+++ flutter_firebase/lib/pages/notifications/notificationMainPage.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/pages/notifications/key.dart';
 import 'package:flutter_firebase/pages/notifications/notification_helper.dart';
+import 'package:http/http.dart' as http;
 
 class NotificationMainPage extends StatefulWidget {
   const NotificationMainPage({super.key});
@@ -22,10 +26,42 @@ class _NotificationMainPageState extends State<NotificationMainPage> {
     super.initState();
   }
 
+  void sendNotification() async {
+    await helper.getDeviceToken().then((value) async {
+      var data = {
+        "to": value
+            .toString(), //to send to another device,replace with device token
+        "priority": "high",
+        "notification": {
+          "title": "Notification Message",
+          "body": "Notification Message from one device to another device",
+          "image":
+              "https://www.pexels.com/photo/woman-posing-for-photo-shoot-1391498/"
+        },
+        "data": {"type": "msg", "id": "testid"}
+      };
+      await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
+          body: jsonEncode(data),
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Authorization': 'key=$key'
+          });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Notification page"),
+      ),
+      body: Center(
+        child: TextButton(
+            onPressed: () {
+              sendNotification();
+            },
+            child: const Text("Send notification")),
+      ),
     );
   }
 }
